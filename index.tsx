58d5f2e5bf0b72fdab2991f31fb66be141d85e07
Mokerrp <mokerrp@gmail.com>
2025-12-15 01:24:26 +0800

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,5 @@
+
+// Main logic is contained in index.html to serve as a standalone application.
+// This file is kept as a valid module entry point if the environment requires it.
+export {};
+    
